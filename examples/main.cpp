fbb55d72f8ce55b17dbd4189d6d3f660d5ad8b39
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2018-08-21 00:00:56 +0200

    Reworked examples a bit.

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,10 +4,44 @@
 #include <iostream>
 #include <vector>
 
-using vec3 = std::array<float, 3>;
+void example_1()
+{
+    using vec3 = std::array<float, 3>;
+
+    using is_active = bool;
+    using position = vec3;
+    struct velocity : public vec3
+    {
+    };
+    using color = uint32_t;
+
+    soacpp::soa_vector<is_active, position, velocity, color> particles( 128 );
+
+    is_active current_is_active = true;
+
+    for ( auto particle : particles )
+    {
+        current_is_active ^= 1;
+
+        // :TRICKY: cannot use bool or alias because type is vector<bool>::reference
+        std::get<0>( particle ) = current_is_active;
+
+        std::get<position &>( particle )[0] = 0.0f;
+        std::get<position &>( particle )[1] = 1.0f;
+        std::get<position &>( particle )[2] = 2.0f;
+
+        std::get<velocity &>( particle )[0] = 3.0f;
+        std::get<velocity &>( particle )[1] = 4.0f;
+        std::get<velocity &>( particle )[2] = 5.0f;
+
+        std::get<color &>( particle ) = 0xFF0000FF;
+    }
+}
 
 struct Particle
 {
+    using vec3 = std::array<float, 3>;
+
     vec3 position;
     uint32_t color;
     bool is_active;
@@ -16,7 +50,7 @@ struct Particle
 
 DECLARE_SOA_TYPE( Particle, is_active, position, color )
 
-int main()
+void example_2()
 {
     static constexpr auto count = 100'000;
 
@@ -42,48 +76,32 @@ int main()
 
         for ( std::size_t j = 0; j < loop_count; ++j )
         {
-            for ( decltype(auto) particle : particles )
+            for ( decltype( auto ) particle : particles )
             {
                 if ( particle.is_active )
                 {
                     particle.position[0] += 3.0f;
                 }
 
-                //std::cout << i << ". pos: {" << particle.position[0] << ", " << particle.position[1] << ", " << particle.position[2] << "}, color: " << particle.color << std::endl;
+                // std::cout << i << ". pos: {" << particle.position[0] << ", " << particle.position[1] << ", " <<
+                // particle.position[2] << "}, color: " << particle.color << std::endl;
             }
         }
 
         auto end = std::chrono::steady_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count() / loop_count << std::endl;
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count() / loop_count
+                  << std::endl;
     };
 
     test( std::vector<Particle>( count ) );
     test( soacpp::soa_vector<Particle>( count ) );
     test( soacpp::soa_array<count, Particle>() );
+}
 
-    // :TODO: support named access to multiple fields with same underlying type (e.g. position, velocity, ...)
-
-    using is_active = bool;
-    using position = vec3;
-    using velocity = vec3;
-    using color = uint32_t;
-    soacpp::soa_vector<is_active, position, velocity, color> particles( count );
-    uint32_t i = 0;
-
-    for ( auto particle : particles )
-    {
-        std::get<0>( particle ) = ( ( i++ % 3 ) != 0u );
-
-        std::get<1>( particle )[0] = 0.0f;
-        std::get<1>( particle )[1] = 1.0f;
-        std::get<1>( particle )[2] = 2.0f;
-
-        // std::get<velocity&>( particle )[0] = 2.0f;
-        // std::get<velocity&>( particle )[1] = 2.0f;
-        // std::get<velocity&>( particle )[2] = 2.0f;
-
-        std::get<color &>( particle ) = 3.0f;
-    }
+int main()
+{
+    example_1();
+    example_2();
 
     return 0;
 }
